d921fc73391152f8d2a0b1f3af9badd6a219b244
AliElMahouli <70891438+AliElMahouli@users.noreply.github.com>
2025-03-18 23:35:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,24 @@ const sectors = [
   { color: "#FF5A10", text: "#333333", label: "Bun" },
 ];
 
-var img=document.createElement("img");
-img.onload=function(){
-       draw();
-}
-img.src="http://images4.fanpop.com/image/photos/23400000/water-water-23444632-2048-1277.jpg";
+const image = new Image(60, 45); // Using optional size for image
+image.onload = drawImageActualSize; 
+image.src="http://images4.fanpop.com/image/photos/23400000/water-water-23444632-2048-1277.jpg";
 
+function drawImageActualSize() {
+  // Use the intrinsic size of image in CSS pixels for the canvas element
+  canvas.width = this.naturalWidth;
+  canvas.height = this.naturalHeight;
+
+  // Will draw the image as 300x227, ignoring the custom size of 60x45
+  // given in the constructor
+  ctx.drawImage(this, 0, 0);
+
+  // To use the custom size we'll have to specify the scale parameters
+  // using the element's width and height properties - lets draw one
+  // on top in the corner:
+  ctx.drawImage(this, 0, 0, this.width, this.height);
+}
 
 const events = {
   listeners: {},
